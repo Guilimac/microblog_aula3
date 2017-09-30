bd3ce680c0768b6129979c006349fc4fe8efbaef
Guilherme de Lima Conceição <guilherme@ztrade.com.br>
2017-09-30 12:49:58 -0300

    crud finalizado

--- server.js
+++ server.js
@@ -3,20 +3,24 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 //jshint
 
+//conexão com o baco de dados
 mongoose.connect('mongodb://localhost:27017/microdb', function(err, res) {
     if (err) return console.log(err);
     console.log('banco conectado');
 });
 
+//instancia de schema
 var Schema = mongoose.Schema;
 
+//schema de usuário para gravação
 var UsuarioSchema = new Schema({
-    email: String,
+    email: { type: String, unique: true },
     senha: String,
     nome: String,
     idade: Number
 });
 
+//definição de model de usuário
 var Usuario = mongoose.model('usuario', UsuarioSchema);
 
 var app = express();
@@ -25,12 +29,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(request, response) {
+    response.send('Oi');
+});
+
+app.get('/usuario', function(request, response) {
+    //retorna usuários cadastrados
     Usuario.find({}, function(err, usuarios) {
         response.json(usuarios);
     });
 });
 
-app.post('/', function(request, response) {
+app.get('/usuario/:nome', function(request, response) {
+    var nome = request.params.nome;
+    Usuario.find({ nome: nome }, function(err, usuarios) {
+        if (err) return response.send(err);
+        response.json(usuarios);
+    });
+});
+
+app.put('/usuario/:id', function(request, response) {
+    var id = request.params.id;
+    Usuario.update({ _id: id }, { $set: request.body }, function(err, result) {
+        if (err) return response.send(err);
+        response.json(result);
+    });
+});
+
+app.delete('/usuario', function(request, response) {
+    var id = request.body.id;
+    Usuario.remove({ _id: id }, function(err, result) {
+        if (err) return response.send(err);
+        response.json(result);
+    });
+});
+
+app.post('/usuario', function(request, response) {
+    //cadastra novo usuário
     var usuario = new Usuario({
         email: request.body.email,
         senha: request.body.senha,
