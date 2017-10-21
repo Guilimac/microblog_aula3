921f31360d8db15048a5a4fc4e51e4609bd8f12f
Guilherme de Lima Conceição <guilherme@ztrade.com.br>
2017-10-21 10:34:47 -0200

    separação de controller

---
+++ api/usuario/controller.js
@@ -0,0 +1,55 @@
+var Usuario = require('./models/usuario');
+
+function UsuarioController(){}
+
+UsuarioController.prototype.getAll = function(request,response){
+    Usuario.find({}, function(err, usuarios) {
+        response.json(usuarios);
+    });
+}
+
+UsuarioController.prototype.getByName = function(request, response) {
+    var nome = request.params.nome;
+    Usuario.find({ nome: nome }, function(err, usuarios) {
+        if (err) return response.send(err);
+        response.json(usuarios);
+    });
+}
+
+UsuarioController.prototype.putUser = function(request, response) {
+    var id = request.params.id;
+    Usuario.update({ _id: id }, { $set: request.body }, function(err, result) {
+        if (err) return response.send(err);
+        response.json(result);
+    });
+}
+
+UsuarioController.prototype.postUser = function(request, response) {
+    var usuario = new Usuario({
+        email: request.body.email,
+        senha: request.body.senha,
+        nome: request.body.nome,
+        idade: request.body.idade
+    });
+    usuario.save(function(err, result) {
+        if (err) response.send(err);
+        response.json(result);
+    });
+}
+
+UsuarioController.prototype.deleteUser = function(request, response) {
+    var id = request.body.id;
+    Usuario.remove({ _id: id }, function(err, result) {
+        if (err) return response.send(err);
+        response.json(result);
+    });
+}
+
+
+module.exports = new UsuarioController();
+
+class Controller(){
+    costructor(){
+        
+    }
+}
--- api/usuario/index.js
+++ api/usuario/index.js
@@ -1,51 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var controller = require('./controller');
 
-var Usuario = require('./models/usuario');
-
-router.get('/', function(request, response) {
-    //retorna usuários cadastrados
-    Usuario.find({}, function(err, usuarios) {
-        response.json(usuarios);
-    });
-});
-
-router.get('/:nome', function(request, response) {
-    var nome = request.params.nome;
-    Usuario.find({ nome: nome }, function(err, usuarios) {
-        if (err) return response.send(err);
-        response.json(usuarios);
-    });
-});
-//atualiza usuário
-router.put('/:id', function(request, response) {
-    var id = request.params.id;
-    Usuario.update({ _id: id }, { $set: request.body }, function(err, result) {
-        if (err) return response.send(err);
-        response.json(result);
-    });
-});
-//deleta um usuário
-router.delete('/', function(request, response) {
-    var id = request.body.id;
-    Usuario.remove({ _id: id }, function(err, result) {
-        if (err) return response.send(err);
-        response.json(result);
-    });
-});
-
-router.post('/', function(request, response) {
-    //cadastra novo usuário
-    var usuario = new Usuario({
-        email: request.body.email,
-        senha: request.body.senha,
-        nome: request.body.nome,
-        idade: request.body.idade
-    });
-    usuario.save(function(err, result) {
-        if (err) response.send(err);
-        response.json(result);
-    });
-});
+router.get('/', controller.getAll);
+router.get('/:nome', controller.getByName);
+router.put('/:id', controller.putUser);
+router.delete('/', controller.deleteUser);
+router.post('/', controller.postUser);
 
 module.exports = router;
